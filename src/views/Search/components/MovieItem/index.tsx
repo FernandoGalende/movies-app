e709a678e53330673bfcad7f154660feebbd259e
FernandoGalende <fernandogalende@gmail.com>
2025-06-07 22:56:02 +0200

    feat(detail): add basic routing

--- src/views/Search/components/MovieItem/index.tsx
+++ src/views/Search/components/MovieItem/index.tsx
@@ -1,15 +1,23 @@
 import type { Movie } from "@/types";
 import { styles } from "./styles";
+import { useNavigate } from "react-router";
 
 interface MovieItemProps {
   movie: Movie;
 }
 
 export function MovieItem({ movie }: MovieItemProps) {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/detail/${movie.id}`);
+  };
+
   return (
     <li key={movie.id} className={styles.movieItem}>
       <article className={styles.movieArticle}>
         <h3 className={styles.movieTitle}>{movie.title}</h3>
+        <button onClick={handleClick}>View details</button>
       </article>
     </li>
   );
