fda1cfb8965e46b7ac2d09c63d01e3f64d7244de
FernandoGalende <fernandogalende@gmail.com>
2025-06-07 13:53:23 +0200

    feat(api): add getMovies fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { getMovies } from "@/api/movies";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    getMovies({ query: "batman", page: "1" }).then((data) => {
+      console.log(data);
+    });
+  }, []);
 
   return (
     <>
@@ -29,7 +36,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
