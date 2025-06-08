0514feedff2dd7bc2a93236619c084b81cf90bc3
FernandoGalende <fernandogalende@gmail.com>
2025-06-08 17:26:39 +0200

    perf(api): optimize movies api performance

--- src/api/movies.ts
+++ src/api/movies.ts
@@ -16,6 +16,9 @@ interface GetMoviesResponse {
   total_results: number;
 }
 
+// cache for movies to avoid multiple requests
+const cacheMovies = new Map<string, GetMoviesResponse>();
+
 export const getMovies = async ({
   query,
   page,
@@ -26,12 +29,31 @@ export const getMovies = async ({
     page: page.toString(),
     include_adult: "false",
   });
+  const cacheKey = `${queryParams.toString()}`;
   const url = `${BASE_URL}/search/movie?${queryParams.toString()}`;
 
-  return fetcher(url);
+  if (cacheMovies.has(cacheKey)) {
+    return cacheMovies.get(cacheKey)!;
+  }
+
+  const data = await fetcher(url);
+  cacheMovies.set(cacheKey, data);
+
+  return data;
 };
+
+// cache for movie details to avoid multiple requests
+const cacheMovie = new Map<string, Movie>();
 
 export const getMovie = async (id: string): Promise<Movie> => {
   const url = `${BASE_URL}/movie/${id}?api_key=${API_KEY}`;
-  return fetcher(url);
+
+  if (cacheMovie.has(id)) {
+    return cacheMovie.get(id)!;
+  }
+
+  const data = await fetcher(url);
+  cacheMovie.set(id, data);
+
+  return data;
 };
