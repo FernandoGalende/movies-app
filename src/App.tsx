0fa20b9efe2de4f5e4a8e1bb43cf3b2f972eae34
FernandoGalende <fernandogalende@gmail.com>
2025-06-19 17:04:03 +0200

    refactor(router): use AppRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
-import { Search, Detail } from "@/views";
-import { BrowserRouter, Routes, Route } from "react-router";
 import "./index.css";
 import { ErrorBoundary } from "./views";
 import { styles } from "./styles";
 import { SearchProvider } from "@/context";
+import { AppRouter } from "./router";
 
 export function App() {
   return (
     <div className={styles.root}>
       <ErrorBoundary>
         <SearchProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Search />} />
-              <Route path="/detail/:id" element={<Detail />} />
-            </Routes>
-          </BrowserRouter>
+          <AppRouter />
         </SearchProvider>
       </ErrorBoundary>
     </div>
